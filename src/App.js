da04bd1619dcbcf8e8564ec8c4b11e3d0489e893
hemanth <saihemanth9490@gmail.com>
2021-06-07 17:51:19 +0530

    simple change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Router>
             <div>
                 <Navbar/>
-                <h1>Hi </h1>
+         
                 <Bodysection/>
                 <Footer/>
             </div>
